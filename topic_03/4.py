3a6eb3a66fcc25675fc21e42ae557299fed8a12e
IlliaBevziuk-CS-241 <likeumomagain@gmail.com>
2025-12-17 20:54:58 +0000

    Update 4.py

--- topic_03/4.py
+++ topic_03/4.py
@@ -1,11 +1,9 @@
 def find_insert_position(sorted_list, value):
-    """Повертає позицію (індекс), куди слід вставити value, щоб зберегти порядок."""
     for i in range(len(sorted_list)):
         if value < sorted_list[i]:
             return i
-    return len(sorted_list)  # вставити в кінець, якщо елемент найбільший
+    return len(sorted_list)
 
-# --- Тест ---
 nums = [1, 3, 5, 7, 9]
 print("Відсортований список:", nums)
 
@@ -14,6 +12,5 @@ pos = find_insert_position(nums, new_val)
 
 print(f"Позиція для вставки: {pos}")
 
-# Показати, як буде виглядати список після вставки
 nums.insert(pos, new_val)
 print("Новий список:", nums)
